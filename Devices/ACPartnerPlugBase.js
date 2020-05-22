2beb030ae440d7228ac2b4834ef66ffaf28ed873
Sambor Gogacz <6859944+esgie@users.noreply.github.com>
2020-05-22 19:15:40 +0200

    Update ACPartnerPlugBase.js

--- Devices/ACPartnerPlugBase.js
+++ Devices/ACPartnerPlugBase.js
@@ -97,7 +97,7 @@ ACPartnerPlugBaseOutlet.prototype.setPower = function(value, callback) {
     });
 }
 
-MiPlugBaseTemperature = function(dThis) {
+ACPartnerPlugBaseOutlet = function(dThis) {
     this.device = dThis.device;
     this.name = dThis.config['temperatureName'];
     this.platform = dThis.platform;
@@ -129,58 +129,6 @@ MiPlugBaseTemperature.prototype.getTemperature = function(callback) {
         callback(null, result[0]);
     }).catch(function(err) {
         that.platform.log.error("[MiOutletPlatform][ERROR]MiPlugBase - Temperature - getTemperature Error: " + err);
-        callback(err);
-    });
-}
-
-MiPlugBaseSwitchLED = function(dThis) {
-    this.device = dThis.device;
-    this.name = dThis.config['switchLEDName'];
-    this.platform = dThis.platform;
-}
-
-MiPlugBaseSwitchLED.prototype.getServices = function() {
-    var services = [];
-
-    var infoService = new Service.AccessoryInformation();
-    infoService
-        .setCharacteristic(Characteristic.Manufacturer, "XiaoMi")
-        .setCharacteristic(Characteristic.Model, "Plug Base")
-        .setCharacteristic(Characteristic.SerialNumber, "Undefined");
-    services.push(infoService);
-    
-    var switchLEDService = new Service.Lightbulb(this.name);
-    switchLEDService
-        .getCharacteristic(Characteristic.On)
-        .on('get', this.getLEDPower.bind(this))
-        .on('set', this.setLEDPower.bind(this));
-    services.push(switchLEDService);
-
-    return services;
-}
-
-MiPlugBaseSwitchLED.prototype.getLEDPower = function(callback) {
-    var that = this;
-    this.device.call("get_prop", ["wifi_led"]).then(result => {
-        that.platform.log.debug("[MiOutletPlatform][DEBUG]MiPlugBase - SwitchLED - getLEDPower: " + result);
-        callback(null, result[0] === 'on' ? true : false);
-    }).catch(function(err) {
-        that.platform.log.error("[MiOutletPlatform][ERROR]MiPlugBase - SwitchLED - getLEDPower Error: " + err);
-        callback(err);
-    });
-}
-
-MiPlugBaseSwitchLED.prototype.setLEDPower = function(value, callback) {
-    var that = this;
-    that.device.call("set_wifi_led", [value ? "on" : "off"]).then(result => {
-        that.platform.log.debug("[MiOutletPlatform][DEBUG]MiPlugBase - SwitchLED - setLEDPower Result: " + result);
-        if(result[0] === "ok") {
-            callback(null);
-        } else {
-            callback(new Error(result[0]));
-        }
-    }).catch(function(err) {
-        that.platform.log.error("[MiOutletPlatform][ERROR]MiPlugBase - SwitchLED - setLEDPower Error: " + err);
         callback(err);
     });
 }
