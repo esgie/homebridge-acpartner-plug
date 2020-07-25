4ec3797d5aa04b97d176e377d58ef90b596559ff
Sambor Gogacz <6859944+esgie@users.noreply.github.com>
2020-07-25 11:31:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-require('./Devices/MiPlugBase');
-require('./Devices/MiPlugBaseWithUSB');
-require('./Devices/MiIntelligencePinboard');
-require('./Devices/MiQingPinboard');
-require('./Devices/MiQingPinboardWithUSB');
-require('./Devices/MiPlugBaseEnhanced');
 require('./Devices/ACPartnerPlugBase');
 
 var fs = require('fs');
@@ -11,7 +5,7 @@ var packageFile = require("./package.json");
 var PlatformAccessory, Accessory, Service, Characteristic, UUIDGen;
 
 module.exports = function(homebridge) {
-    if(!isConfig(homebridge.user.configPath(), "platforms", "MiOutletPlatform")) {
+    if(!isConfig(homebridge.user.configPath(), "platforms", "AcPartnerPlugPlatform")) {
         return;
     }
     
@@ -21,7 +15,7 @@ module.exports = function(homebridge) {
     Characteristic = homebridge.hap.Characteristic;
     UUIDGen = homebridge.hap.uuid;
 
-    homebridge.registerPlatform('homebridge-mi-outlet', 'MiOutletPlatform', MiOutletPlatform, true);
+    homebridge.registerPlatform('homebridge-acpartner-plug', 'AcPartnerPlugPlatform', AcPartnerPlugPlatform, true);
 }
 
 function isConfig(configFile, type, name) {
@@ -46,7 +40,7 @@ function isConfig(configFile, type, name) {
     return false;
 }
 
-function MiOutletPlatform(log, config, api) {
+function AcPartnerPlugPlatform(log, config, api) {
     if(null == config) {
         return;
     }
@@ -63,13 +57,6 @@ function MiOutletPlatform(log, config, api) {
     if (api) {
         this.api = api;
     }
-    
-    this.log.info("[MiOutletPlatform][INFO]**************************************************************");
-    this.log.info("[MiOutletPlatform][INFO]          MiOutletPlatform v%s By YinHang", packageFile.version);
-    this.log.info("[MiOutletPlatform][INFO]  GitHub: https://github.com/YinHangCode/homebridge-mi-outlet ");
-    this.log.info("[MiOutletPlatform][INFO]                                         QQ Group: 107927710  ");
-    this.log.info("[MiOutletPlatform][INFO]**************************************************************");
-    this.log.info("[MiOutletPlatform][INFO]start success...");
 }
 
 MiOutletPlatform.prototype = {
@@ -80,40 +67,12 @@ MiOutletPlatform.prototype = {
         if(deviceCfgs instanceof Array) {
             for (var i = 0; i < deviceCfgs.length; i++) {
                 var deviceCfg = deviceCfgs[i];
-                if(null == deviceCfg['type'] || "" == deviceCfg['type'] || null == deviceCfg['token'] || "" == deviceCfg['token'] || null == deviceCfg['ip'] || "" == deviceCfg['ip']) {
+                if(null == deviceCfg['token'] || "" == deviceCfg['token'] || null == deviceCfg['ip'] || "" == deviceCfg['ip']) {
                     continue;
                 }
-                
-                if (deviceCfg['type'] == "MiPlugBase") {
-                    new MiPlugBase(this, deviceCfg).forEach(function(accessory, index, arr){
-                        myAccessories.push(accessory);
-                    });
-                } else if (deviceCfg['type'] == "MiPlugBaseWithUSB") {
-                    new MiPlugBaseWithUSB(this, deviceCfg).forEach(function(accessory, index, arr){
-                        myAccessories.push(accessory);
-                    });
-                } else if (deviceCfg['type'] == "MiIntelligencePinboard") {
-                    new MiIntelligencePinboard(this, deviceCfg).forEach(function(accessory, index, arr){
-                        myAccessories.push(accessory);
-                    });
-                } else if (deviceCfg['type'] == "MiQingPinboard") {
-                    new MiQingPinboard(this, deviceCfg).forEach(function(accessory, index, arr){
-                        myAccessories.push(accessory);
-                    });
-                } else if (deviceCfg['type'] == "MiQingPinboardWithUSB") {
-                    new MiQingPinboardWithUSB(this, deviceCfg).forEach(function(accessory, index, arr){
-                        myAccessories.push(accessory);
-                    });
-                } else if (deviceCfg['type'] == "MiPlugBaseEnhanced") {
-                    new MiPlugBaseEnhanced(this, deviceCfg).forEach(function(accessory, index, arr){
-                        myAccessories.push(accessory);
-                    });
-                } else if (deviceCfg['type'] == "ACPartnerPlugBase") {
-                    new ACPartnerPlugBase(this, deviceCfg).forEach(function(accessory, index, arr){
-                        myAccessories.push(accessory);
-                    });
-                } else {
-                }
+                new ACPartnerPlugBase(this, deviceCfg).forEach(function(accessory, index, arr){
+                    myAccessories.push(accessory);
+                });
             }
             this.log.info("[MiOutletPlatform][INFO]device size: " + deviceCfgs.length + ", accessories size: " + myAccessories.length);
         }
